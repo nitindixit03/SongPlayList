6a81c8f9ec6d6ba8d3f7a00457ad53ce3f8080bd
Nitin Dixit <nitinsdixit03@gmail.com>
2024-06-20 01:51:34 +0530

    small issue solved

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("Welcome to Spotify");
 
 let songIndex = 0;
-let audioElement = new Audio();
+let audioElement = new Audio('songs/1.mp3');
 let masterPlay = document.getElementById("masterPlay");
 let myProgressBar = document.getElementById("myProgressBar");
 let gif = document.getElementById("gif");
